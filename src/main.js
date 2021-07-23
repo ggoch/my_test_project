9af8105dfc60bf38400fb63fdb52335cb23c0c50
ggoch <loyuqingan@gmail.com>
2021-07-23 11:53:44 +0800

    import vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vuex from 'vuex'
 Vue.config.productionTip = false
 
 
@@ -8,5 +9,6 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
   router,
+  Vuex,
 }).$mount('#app');
 
