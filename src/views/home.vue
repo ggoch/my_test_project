fa20ffd2b8f945ff53c4a624e928f6f49836dee1
ggoch <loyuqingan@gmail.com>
2021-08-25 11:14:38 +0800

    home scss add

--- src/views/home.vue
+++ src/views/home.vue
@@ -7,7 +7,7 @@
               <li><a href="#"><img src="../assets/sideshow1.jpg" alt="" /></a></li>
               <li><a href="#"><img src="../assets/sideshow2.jpg" alt="" /></a></li>
               <li><a href="#"><img src="../assets/sideshow3.jpg" alt="" /></a></li>
-              <img class="rwdimg" src="../assets/sideshow1.jpg" alt="" />
+              <img src="../assets/sideshow1.jpg" alt="" />
             </ul>
           </div>
           <div class = "category">
@@ -50,42 +50,80 @@ export default{
 
 </script>
 <style lang="scss">
-.main-header .sideshow{     //首頁幻燈片區塊
-    width:100%;
-    display:flex;
-    justify-content:center;
-    //background:green;
-}
-.sideshow ul{
-    width:620px;
-    height:320px;
-    display:flex;
-    //background:blue;
-    position:relative;
-}
-.sideshow ul>img{
-    width:100%;
-    height:100%;
-    visibility:hidden;
-}
-.main-header .sideshow li {
-    //display:block;
-    position:absolute;
-    top:0;
-    left:0;
-    opacity:0;
-    max-width:620px;
-    max-height:320px;
-    width:100%;
-    height:100%;
-    //background:green;
-    filter:alpha(opacity=0);
-    animation:silder 15s linear infinite;
-}
-.sideshow li img{
-    //object-fit:cover;
-    width:100%;
-    height:100%;
+.main-header{                          //首頁幻燈片區塊
+    .sideshow{
+        width:620px;
+        margin:auto;    
+        ul{
+            width:100%;
+            display:flex;
+            position:relative;    
+            >img{
+                width:100%;
+                visibility:hidden;
+            }    
+            li{
+                position:absolute;
+                top:0;
+                left:0;
+                opacity:0;
+                width:100%;
+                height:100%;
+                filter:alpha(opacity=0);
+                animation:silder 15s linear infinite;    
+                img{
+                    width:100%;
+                }    
+                &:nth-child(1){
+                    animation-delay:0s;
+                }
+                &:nth-child(2){
+                    animation-delay:5s;
+                }
+                &:nth-child(3){
+                    animation-delay:10s;
+                }
+            }
+        }
+    }
+
+    .category{
+        background:#0072E3;
+        padding-left:10%;
+
+        >input[type="checkbox"],
+        .homeMenu{
+            display:none;
+        }
+
+        .category-list{
+            display:flex;
+            a{
+                color:#fff;
+                font-size:16px;
+                font-weight:bold;
+                margin:0 20px;
+                display:flex;
+                align-items:center;
+                padding:10px;
+                height:50px;
+                transition:0.5s;
+
+                &:hover{
+                    background:#46A3FF;
+                }
+            }
+        }
+
+        ul.totel .totel,
+        ul.game .game,
+        ul.boutique .boutique,
+        ul.book .book{
+            background:#2828FF;
+            color:#FFFF37;
+            border-bottom:3px solid #FFFF37;
+        }
+    }
 }
 @keyframes silder{
     7%{
@@ -101,149 +139,49 @@ export default{
         filter:alpha(opacity=0);
     }
 }
-.main-header .sideshow li:nth-child(1){
-    animation-delay:0s;
-}
-.main-header .sideshow li:nth-child(2){
-    animation-delay:5s;
-}
-.main-header .sideshow li:nth-child(3){
-    animation-delay:10s;
-}
-/*.main-header{
-    .sideshow{
-        width:100%;
-        display:flex;
-        justify-content:center;
-        //background:green;
-
-        ul{
-            width:620px;
-            height:320px;
-            display:flex;
-            //background:blue;
-            position:relative;
-
-            .rwdimg{
-                width:100%;
-                height:100%;
-                visibility:hidden;
-            }
-
-            li{
-                position:absolute;
-                top:0;
-                left:0;
-                opacity:0;
-                max-width:620px;
-                max-height:320px;
-                width:100%;
-                height:100%;
-                filter:alpha(opacity=0);
-                animation:silder 15s linear infinite;
-
-                img{
-                    width:100%;
-                    height:100%;
-                }
-
-                &:nth-child(1){
-                    animation-delay:0s;
-                }
-                &:nth-child(2){
-                    animation-delay:5s;
-                }
-                &:nth-child(3){
-                    animation-delay:10s;
-                }
-            }
-        }
-    }
-}*/
-@media(max-width:420px){       /*幻燈片rwd*/
-    .sideshow ul{
-      width:100%;
-      height:auto;
-      font-size:0;
-    }
-    .sideshow li img{
-      object-fit:cover;
-      //width:100%;
-    }
-
-    /*.sideshow{
-        ul{
+@media(max-width:620px){       /*幻燈片rwd*/
+    .main-header{
+        .sideshow{
             width:100%;
-            height:auto;
-            font-size:0;
+        }
+    }
+}
 
-            li{
-                img{
-                    object-fit:cover;
-                    //width:100%;
+@media(max-width:576px){         /*首頁商品分類rwd*/
+    .main-header{
+        .category{
+            position:relative;
+            padding-left:20px;
+    
+            .homeMenu{
+                display:flex;
+                align-item:center;
+                padding:10px;
+    
+                &:hover{
+                    cursor:pointer;
                 }
             }
-        }
-    }*/
-}
-.category{
-    background:#0072E3;
-    padding-left:100px;
-}
-.category>input[type="checkbox"],
-.category .homeMenu{
-    display:none
-}
-.category .category-list{
-    display:flex;
-}
-.category-list a{
-    color:#fff;
-    font-size:17px;
-    font-weight:bold;
-    margin:0 20px;
-    display:flex;
-    align-items:center;
-    padding:10px;
-    height:50px;
-    transition:0.5s;
-}
-.category-list a:hover{
-    background:#46A3FF;
-}
-@media(max-width:550px){         /*首頁商品分類rwd*/
-    .category{
-        position:relative;
-        padding-left:20px;
+    
+            >input{
+                &:checked ~ .category-list{
+                        transform:translateX(0);
+                }
+            }
+    
+            .category-list{
+                flex-flow:column;
+                position:absolute;
+                left:-20px;
+                transform:translateX(-100%);
+                transition:0.3s;
+                a{
+                    font-size:14px;
+                    background:#0072E3;
+                }
+            }
+        }    
     }
-    .category .homeMenu{
-        display:flex;
-        align-item:center;
-        padding:10px;
-    }
-    .category .homeMenu:hover{
-        cursor:pointer;
-    }
-    .category>input:checked ~ .category-list{
-        transform:translateX(0);
-    }
-    .category .category-list{
-        flex-flow:column;
-        position:absolute;
-        left:-20px;
-        transform:translateX(-100%);
-        transition:0.3s;
-    }
-    .category-list a{
-        font-size:14px;
-        background:#0072E3;
-    }
-}
-ul.totel .totel,
-ul.game .game,
-ul.boutique .boutique,
-ul.book .book{
-    background:#FF2D2D;
 }
 .main-container{
     display:flex;
