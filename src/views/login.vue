25b2153fb6733921bc139b5287157ebd64b865f2
ggoch <loyuqingan@gmail.com>
2021-08-25 13:30:03 +0800

    login scss add

--- src/views/login.vue
+++ src/views/login.vue
@@ -73,140 +73,176 @@ export default{
 }
 </script>
 
-<style>
-#login form{
+<style lang="scss">
+#login .container{
+  form{
     width:1000px;
-    max-width:100%;
+    max-width:95%;
+    //height:450px;
     display:flex;
     margin:50px auto;
     padding:15px;
     border:5px solid #BEBEBE;
-}
-form .data-img{
-    width:60%;
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    border-right:1px solid #BEBEBE;
-}
-form .data-img img{
-    max-width:500px;
-    max-height:400px;
-}
-form .login-data{
-    width:40%;
-    display:flex;
-    flex-flow:column;
-    //padding:10px;
-}
-.login-data .data{
-    flex:1;
-    display:flex;
-    flex-flow:column;
-    justify-content:space-around;
-    align-items:center;
 
-}
-.login-data .data h3{
-  align-self:flex-start;
-  padding-left:10px;
-}
-.login-data .data input{
-    width:300px;
-    max-width:100%;
-    height:35px;
-}
-.login-data .login-button{
-    flex:1;
-    display:flex;
-    flex-flow:column;
-}
-.login-button div{
-    flex:1;
-    display:flex;
-    justify-content:center;
-    align-items:center;
-}
-.login-button div:first-child input{
-    width:150px;
-    max-width:100%;
-    height:40px;
-    font-size:16px;
-    color:#fff;
-    background:#2894FF;
-    transition:0.4s;
-}
-.login-button div:first-child input:hover{
-  background:#0072E3;
-  box-shadow: 0px 1px 2px #BEBEBE;
-}
-.login-button div:last-child input:last-child{
-  margin-left:20px;
-}
-.login-button div input{
-    width:100px;
-    max-width:100%;
-    height:30px;
-    font-weight:bold;
-    color:#ECECFF;
-    border:none;
-    background:#97CBFF;
-    cursor:pointer;
-    border-radius:3px;
-    transition:0.4s;
-    box-shadow:0 3px 5px #BEBEBE;
-}
-.login-button div input:hover{
-  box-shadow: 0px 1px 2px #BEBEBE;
-}
-.login-data .remind{
-    flex:1;
-    padding:0 40px;
-}
-.login-data .remind p{
-    font-size:14px;
-    line-height:24px;
-    background:#FFDAC8;
-    color:#5B5B5B;
+    .data-img{
+      width:60%;
+      display:flex;
+      justify-content:center;
+      align-items:center;
+      border-right:1px solid #BEBEBE;
+
+      img{
+        //max-width:500px;
+        width:63%;
+        max-height:400px;
+      }
+    }
+
+    .login-data{
+      width:40%;
+      display:flex;
+      flex-flow:column;
+
+      .data{
+        //flex:1;
+        height:33%;
+        display:flex;
+        flex-flow:column;
+        justify-content:space-around;
+        align-items:center;
+
+        h3{
+          align-self:flex-start;
+          padding-left:10px;
+        }
+
+        input{
+          width:78%;
+          //max-width:100%;
+          height:35px;
+        }
+      }
+
+      .login-button{
+        //flex:1;
+        height:33%;
+        display:flex;
+        flex-flow:column;
+        div{
+          flex:1;
+          display:flex;
+          justify-content:center;
+          align-items:center;
+          &:first-child input{
+            width:150px;
+            max-width:100%;
+            height:40px;
+            font-size:16px;
+            color:#fff;
+            background:#2894FF;
+            transition:0.4s;
+            &:hover{
+              background:#0072E3;
+              box-shadow: 0px 1px 2px #BEBEBE;
+            }
+          }
+          &:last-child input:last-child{
+            margin-left:20px;
+          }
+          input{
+            width:100px;
+            max-width:100%;
+            height:30px;
+            font-weight:bold;
+            color:#ECECFF;
+            border:none;
+            background:#97CBFF;
+            cursor:pointer;
+            border-radius:3px;
+            transition:0.4s;
+            box-shadow:0 3px 5px #BEBEBE;
+            &:hover{
+              box-shadow: 0px 1px 2px #BEBEBE;
+            }
+          }
+        }
+      }
+
+      .remind{
+        //flex:1;
+        height:33%;
+        padding:0 40px;
+        p{
+          font-size:14px;
+          line-height:24px;
+          background:#FFDAC8;
+          color:#5B5B5B;
+        }
+      }
+    }
+  }
 }
 @media screen and (min-width:600px) and (max-width:900px){  /*登入頁面rwd*/
-  #login form{
-    margin:30px auto;
-  }
-  form .data-img img{
-    max-width:400px;
-    max-height:300px;
-  }
-  .login-data .data input{
-    width:200px;
-    height:25px;
-  }
-  .login-button div:first-child input{
-    width:120px;
-    height:30px;
-  }
-  .login-button div input{
-    width:80px;
-    height:20px;
-  }
-  .login-data .remind{
-    padding:0 20px;
-  }
-  .login-data .remind p{
-    font-size:12px;
-    line-height:20px;
+  #login .container{
+    form{
+      margin:30px auto;
+
+      .data-img{
+        img{
+          //max-width:400px;
+          //max-weight:300px;
+        }
+      }
+
+      .login-data{
+        .data{
+          input{
+            width:200px;
+            height:25px;
+          }
+        }
+
+        .login-button{
+          div{
+            &:first-child input{
+              width:120px;
+              height:30px;
+            }
+  
+            input{
+              width:80px;
+              height:20px;
+            }
+          }
+        }
+
+        .remind{
+          padding:0 20px;
+          p{
+            font-size:12px;
+            line-height:20px;
+          }
+        }
+      }
+    }
   }
 }
 @media(max-width:600px){
-  form .data-img{
-    display:none;
-  }
-  form .login-data{
-    width:100%;
-    height:350px;
-  }
-  .login-data .remind{
-    padding:0;
+  #login .container{
+    form{
+      .data-img{
+        display:none;
+      }
+
+      .login-data{
+        width:100%;
+        height:350px;
+
+        .remind{
+          padding:0;
+        }
+      }
+    }
   }
 }
 </style>
